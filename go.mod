b0e61569ebe0d2c6f3d8e14a2c640df8d27c95d1
Cristina Mendes Medeiros do Vale <cristinamedeiros183@gmail.com>
2025-10-21 15:04:47 -0300

    terceiro

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cristinamedeiros183/puppy
 
 go 1.25.3
+
+require github.com/cristinamedeiros183/dog v0.0.0-20251021135408-84d9f96b6308 // indirect
---
+++ puppy.go
@@ -0,0 +1,21 @@
+package puppy
+
+import (
+	"github.com/cristinamedeiros183/dog"
+)
+
+func Bark() string {
+	return "Woof!"
+}
+
+func Barks() string {
+	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrowUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrowUp(Barks())
+}
